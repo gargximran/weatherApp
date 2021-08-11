ccc5d5874944f472ffb18bc93e51cd80a53d2b82
gargximran <gargximran@gmail.com>
2021-08-11 18:39:18 +0600

    c

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -1,3 +1,3 @@
 module.exports = {
-    weatherUrl: 'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40'
+    weatherUrl: 'https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40'
 }
